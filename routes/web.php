f1dac77f191e4063a91fd52f424c0bbaace1e6ae
fanzhuo <fans1992@qq.com>
2018-08-04 15:27:31 +0000

    Topic order

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 
-
-
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 //Route::get('/users/{user}', 'UsersController@show')->name('users.show');
@@ -40,5 +38,17 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
 
-
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+
+
+
+//Route::get('/users', 'UsersController@index')->name('users.index');
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/create', 'UsersController@create')->name('users.create');
+//Route::post('/users', 'UsersController@store')->name('users.store');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
